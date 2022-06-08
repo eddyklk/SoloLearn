d01f036d11f11bf4429bc4b29abca1a11f6d045a
Eddy Junior Valerio Gerardino <eddy.valerio@nordestana.com>
2022-06-08 17:30:24 -0400

    casi

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalPrice = Convert.ToInt32(Console.ReadLine());
+
+            Discount(totalPrice);
+
+
+        }
+        //complete the method declaration
+        static double Discount(double x)
+        {
+            if (x >= 1000 )
+            {
+                double a = 0;
+
+                a = 20.0f / 100;
+                x = x - (a/x) ;
+                return x;
+            }
+
+        }
+    }
+}
